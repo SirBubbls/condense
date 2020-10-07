24fab0d0c1b69694c0682ab411d10db4018c4018
SirBubbls <lucassas@live.de>
2020-10-07 14:12:29 +0200

    setup.py changes and pypi description

---
+++ pypi-description.md
@@ -0,0 +1,8 @@
+> **CAUTION**  
+> This module is currently in development so bugs will happen and frequent updates are highly recommended.
+
+# Condense
+
+## Functionality
+- Keras wrapper to prune model during training (`.fit()`)
+- Unit/Weight pruning on numpy ndarrays 
--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'pypi-description.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='condense',
     packages=['condense'],
-    version='0.02',
+    version='0.1.1',
     license='MIT',
     description='Neural Network Pruning Framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Lucas Sas Brunschier',
     author_email='lucassas@live.de',
     url='https://github.com/SirBubbls/condense',
     download_url='https://github.com/SirBubbls/condense/archive/v_01.tar.gz',
     keywords=['pruning', 'ai', 'machine learning', 'tensorflow', 'framework'],
+    python_requires='>=3.8',
     install_requires=[
         'keras',
         'tensorflow',
         'numpy'
+    ],
+    tests_require=[
+        'pillow',
+        'tensorflow_datasets'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
